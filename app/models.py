c7819872108ac69932a9b34ec7b10faee030ae6e
Munkhzul Khurelbaatar <zula@ramones.desktop.ucsf.bkslab.org>
2019-10-01 13:54:26 -0700

    step6 done-profile page and avatars

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
@@ -10,6 +11,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -20,6 +23,9 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return  check_password_hash(self.password_hash, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
