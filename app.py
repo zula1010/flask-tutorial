88df54433c94d48d9a9f6053bbf46986ac910294
Munkhzul Khurelbaatar <zula@ramones.desktop.ucsf.bkslab.org>
2019-09-30 17:46:34 -0700

    step3 done

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from config import Config
+from forms import LoginForm
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 
 @app.route('/')
 @app.route('/index')
@@ -18,6 +20,15 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me {form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 if __name__ == '__main__':
