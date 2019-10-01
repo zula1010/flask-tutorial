f528d2298734e68d9d8c769bffc696508ce05245
Munkhzul Khurelbaatar <zula@ramones.desktop.ucsf.bkslab.org>
2019-09-30 17:56:57 -0700

    SQLite db created

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, flash, redirect, url_for
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from forms import LoginForm
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 @app.route('/index')
