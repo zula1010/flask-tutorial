7f5fa9957a19fc8d00ada11e345f519b27f8974a
Munkhzul Khurelbaatar <zula@ramones.desktop.ucsf.bkslab.org>
2019-10-02 14:24:00 -0700

    step10 done with asynchronous email

--- app/email.py
+++ app/email.py
@@ -1,19 +1,25 @@
 from flask_mail import Message
 from app import mail, app
 from flask import render_template
+from threading import Thread
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    print(f'message created ready to send {msg}')
-    mail.send(msg)
+    # print(f'message created ready to send {msg}')
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    print(f'to {user.email} ')
+    # print(f'to {user.email} ')
     send_email('[Microblog] Reset Your Password',
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
