ad23a28538629caaccc671708b79dcb5ec921d10
Munkhzul Khurelbaatar <zula@ramones.desktop.ucsf.bkslab.org>
2019-10-02 15:30:54 -0700

    step 12 done

--- app/routes.py
+++ app/routes.py
@@ -27,7 +27,7 @@ def index():
         if posts.has_next else None
     prev_url = url_for('explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template("index.html", title='Explore', posts=posts.items,
+    return render_template("index.html", title='Explore', posts=posts.items, form=form,
                            next_url=next_url, prev_url=prev_url)
 
 
